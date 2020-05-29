d0df3f786d01b17dbf91d9d45982d7d8bb644a5c
Scott Davies <scottdavies1983@gmail.com>
2020-05-29 15:19:44 +0100

    initial set up

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+ 
+ class Game {
+   constructor() {
+     this.missed = 0;
+     this.phrases = this.createPhrases();
+     this.activePhrase = 'null';
+   }
+   
+   createPhrases() {
+     const phrases = [
+       { phrase: "Cat got your tongue"},
+       { phrase: "Laughter is the best medicine" },
+       { phrase: "What goes around comes around" },
+       { phrase: "Dont cry over spilled milk"},
+       { phrase: "A diamond in the rough"}
+       
+     ]
+     return phrases;
+   
+   }
+ }
+ 
+ 
