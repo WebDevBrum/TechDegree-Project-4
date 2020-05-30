8953e7d522ac465075f0d5c84bec444e89463737
Scott Davies <scottdavies1983@gmail.com>
2020-05-30 09:37:30 +0100

    work on phrase addition

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,67 @@
    constructor(phrase) {
      this.phrase = phrase.toLowerCase();
    }
+   
+   /** * Display phrase on game board */
+   
+   addPhraseToDisplay() {
+   
+     function createElement(elementName) {
+       const element = document.createElement(elementName);
+       return element;
+     }
+   
+    const phraseDiv = document.getElementById("phrase");
+   const phraseList = phraseDiv.querySelector("ul");
+    
+    
+   
+     //const div = createElement('div');
+   //  const list = createElement('ul')';
+     //const listItem = createElement('li');
+ //    div.className = "section";
+ //    div.id = "phrase";
+//     div.appendChild(list);
+     
+     for (let i = 0; i < this.phrase.length; i++){
+     
+     
+     if(this.phrase.charAt(i) !== '') {
+      let listItem = createElement('li');
+      listItem.className = "hide letter " + this.phrase.charAt(i);
+      listItem.innerHTML = this.phrase.charAt(i);
+      phraseList.appendChild(listItem);
+     
+     } else if (this.phrase.charAt(i) === '') {
+       let listItem = createElement('li');
+       listItem.className = "space";
+       phraseList.appendChild(listItem);
+     
+     }
+     
+     
+     }
+       
+   };
  }
+ 
+ 
+ /**
+ 
+ <div id="phrase" class="section">
+    <ul>
+        <li class="hide letter h">h</li>
+        <li class="hide letter o">o</li>
+        <li class="hide letter w">w</li>
+        <li class="space"> </li>
+        <li class="hide letter a">a</li>
+        <li class="hide letter r">r</li>
+        <li class="hide letter e">e</li>
+        <li class="space"> </li>
+        <li class="hide letter y">y</li>
+        <li class="hide letter o">o</li>
+        <li class="hide letter u">u</li>
+    </ul>
+</div>
+
+**/
--- js/app.js
+++ js/app.js
@@ -6,16 +6,25 @@
  //const game = new Game();
  
  //const phrase = new Phrase('Life is like a box of chocolates'); 
- //console.log(`Phrase - phrase: ${phrase.phrase}`);
+//console.log(`Phrase - phrase: ${phrase.phrase}`);
  
- //const game = new Game();
+// const game = new Game();
 
 //game.phrases.forEach((phrase, index) => { console.log(`Phrase ${index} - phrase: ${phrase.phrase}`); });
 
 //console.log(Game.phrases.length);
 
-const logPhrase = (phrase) => { console.log(`Phrase - phrase: `, phrase.phrase); };
+//const logPhrase = (phrase) => { console.log(`Phrase - phrase: `, phrase.phrase); };
 
-const game = new Game();
+//const game = new Game();
 
-logPhrase(game.getRandomPhrase()); logPhrase(game.getRandomPhrase()); logPhrase(game.getRandomPhrase()); logPhrase(game.getRandomPhrase()); logPhrase(game.getRandomPhrase());
+//logPhrase(game.getRandomPhrase()); logPhrase(game.getRandomPhrase()); logPhrase(game.getRandomPhrase()); logPhrase(game.getRandomPhrase()); logPhrase(game.getRandomPhrase());
+
+//const game = new Game();
+ 
+//game.getRandomPhrase().addPhraseToDisplay();
+
+//game.addPhraseToDisplay();
+
+
+const game = new Game(); const randomPhrase = game.getRandomPhrase(); const phrase = new Phrase(randomPhrase.phrase); phrase.addPhraseToDisplay();
